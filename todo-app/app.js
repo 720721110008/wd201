8f545e44add248d1f34195b811731746591978b6
720721110008 <113924940+720721110008@users.noreply.github.com>
2023-10-04 12:15:13 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -3,6 +3,10 @@ const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const path=require("path")
+app.use(bodyParser.json());
+app.useexpress.static(path.join(_dirname,"public")));
+app.set("view engine","ejs");
 
 app.get("/", function (request, response) {
   response.send("Hello World");
