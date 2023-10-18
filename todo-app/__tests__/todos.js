0a16a94fad365384b748b386288f07cd1aa59fd5
720721110008 <113924940+720721110008@users.noreply.github.com>
2023-10-18 12:09:09 +0530

    Update todos.js

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -87,6 +87,6 @@ describe("Todo Application", function () {
     const deleteResponse = await agent.delete(`/todos/${todoID}`).send({
       _csrf: csrfToken,
     });
-    expect(deleteResponse.statusCode).toBe(302);
+    expect(deleteResponse.statusCode).toBe(200);
   });
 });
